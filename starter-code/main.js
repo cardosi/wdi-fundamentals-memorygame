cfa41107b47ddeb7b9abc47fe97eb776323035ba
CCAZ88 <camcardosi@mac.com>
2016-10-22 11:38:40 -0700

    added simple game logic to js

--- starter-code/main.js
+++ starter-code/main.js
@@ -2,3 +2,12 @@ var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
+
+var clickOne = cardOne;
+var clickTwo = cardThree;
+
+if(clickOne === clickTwo) {
+  alert("You found a match!");
+} else {
+  alert("Whoops, those don't match. Try again.");
+}
