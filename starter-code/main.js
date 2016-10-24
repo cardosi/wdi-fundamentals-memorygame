511cc61da7148a37409acb83161fc0facc401c10
CCAZ88 <camcardosi@mac.com>
2016-10-24 15:04:33 -0700

    added js to create board

--- starter-code/main.js
+++ starter-code/main.js
@@ -11,3 +11,12 @@ if(clickOne === clickTwo) {
 } else {
   alert("Whoops, those don't match. Try again.");
 }
+
+var gameBoard = document.getElementById('game-board');
+function createBoard() {
+	for(var i=0; i < cards.length; i++) {
+		var cardElement = document.createElement('div');
+		cardElement.className = 'card';
+		board.appendChild(cardElement);
+	}
+}
